23dd79730816aef1b727ed7ad9e992a752ad5823
Portatil <gonzalezmoraleseugenio@gmail.com>
2022-04-26 11:14:34 +0200

    Mejor JS

--- codigo/main.js
+++ codigo/main.js
@@ -1,11 +1,16 @@
 
-// Clear function
+// Minigame Number Selector
 var number = random();
 
+// Round
+var round = 1;
+
+// Random 1-4 numbers
 function random(){
     return Math.floor(Math.random() * 4);
 }
 
+// Clear function
 function clear(){
     while (document.body.firstChild){
         document.body.removeChild(document.body.firstChild);
@@ -14,6 +19,7 @@ function clear(){
 
 // Intro function
 function intro(){
+    round = 1;
     //Parts of the page
     var page = document.body;
 
@@ -44,9 +50,7 @@ function intro(){
 }
 
 // Start game function
-var round = 1;
 function play(){
-
     var page = document.body;
 
     clear();
@@ -65,6 +69,7 @@ function play(){
 
     var pausebtn = document.createElement('img');
     pausebtn.setAttribute('src', 'img/ui/pause.png');
+    pausebtn.setAttribute('onclick', 'pause();')
 
     var gameDiv = document.createElement('div');
     gameDiv.setAttribute('class', 'game');
@@ -81,6 +86,44 @@ function play(){
 
 }
 
+// Pause function
+function pause(){
+    var page = document.body;
+
+    var pauseDiv = document.createElement('div');
+    pauseDiv.setAttribute('class', 'pauseBox');
+    pauseDiv.setAttribute('id', 'pauseBox');
+
+    var continueBtn = document.createElement('img');
+    continueBtn.setAttribute('src', 'img/ui/continue.png');
+    continueBtn.setAttribute('onclick', 'closeMenu();')
+
+    var restartBtn = document.createElement('img');
+    restartBtn.setAttribute('src', 'img/ui/restart.png');
+    restartBtn.setAttribute('onclick', 'restart();')
+
+    var toMenuBtn = document.createElement('img');
+    toMenuBtn.setAttribute('src', 'img/ui/back.png');
+
+    page.appendChild(pauseDiv);
+    pauseDiv.appendChild(continueBtn);
+    pauseDiv.appendChild(restartBtn);
+    pauseDiv.appendChild(toMenuBtn);
+}
+
+// Continue function
+function closeMenu(){
+    var pauseDiv = document.getElementById('pauseBox');
+    document.body.removeChild(pauseDiv);
+}
+
+// Restart function
+function restart(){
+    round = 1;
+    play();
+}
+
+// SELECT GAME
 function selectGame(){
     switch (number){
         case 1:
@@ -101,46 +144,64 @@ function selectGame(){
             break;
         }
 }
-
+// DESENRROLLA
 function desenrrollaGame(){
-    var winCondition = false;
-
     var gameDiv = document.getElementById('game');
-
     var des = document.createElement('img');
     des.setAttribute('src', 'img/des/initial.png');
     des.setAttribute('id', 'gamePlaying');
     des.setAttribute('onclick', 'firstDes();')
-
-
     gameDiv.appendChild(des);
-
 }
+    function firstDes(){
+        var des = document.getElementById('gamePlaying');
+        des.setAttribute('src', 'img/des/first.gif');
+        des.setAttribute('onclick', 'secondDes();');
+    }
+    function secondDes(){
+        var des = document.getElementById('gamePlaying');
+        des.setAttribute('src', 'img/des/second.gif');
+        des.setAttribute('onclick', 'thirdDes();');
+    }
+    function thirdDes(){
+        var des = document.getElementById('gamePlaying');
+        des.setAttribute('src', 'img/des/third.gif');
+        setTimeout(() => {nextGame();}, 2000);
+    }
 
-function firstDes(){
-    var des = document.getElementById('gamePlaying');
-
-    des.setAttribute('src', 'img/des/first.gif');
-    des.setAttribute('onclick', 'secondDes();');
+//RETUERCE
+function retuerceGame(){
+    var gameDiv = document.getElementById('game');
+    var des = document.createElement('img');
+    des.setAttribute('src', 'img/ret/initial.png');
+    des.setAttribute('id', 'gamePlaying');
+    des.setAttribute('onclick', 'firstRet();')
+    gameDiv.appendChild(des);
 }
+    function firstRet(){
+        var des = document.getElementById('gamePlaying');
+        des.setAttribute('src', 'img/ret/first.gif');
+        des.setAttribute('onclick', 'secondRet();');
+    }
+    function secondRet(){
+        var des = document.getElementById('gamePlaying');
+        des.setAttribute('src', 'img/ret/second.gif');
+        des.setAttribute('onclick', 'thirdRet();');
+    }
+    function thirdRet(){
+        var des = document.getElementById('gamePlaying');
+        des.setAttribute('src', 'img/ret/third.gif');
+        des.setAttribute('onclick', 'fourthRet');
+    }
+    function fourthRet(){
+        var des = document.getElementById('gamePlaying');
+        des.setAttribute('src', 'img/ret/third.gif');
+        setTimeout(() => {nextGame();}, 2000);
+    }
 
-function secondDes(){
-    var des = document.getElementById('gamePlaying');
-
-    des.setAttribute('src', 'img/des/second.gif');
-    des.setAttribute('onclick', 'thirdDes();');
-}
-
-function thirdDes(){
-    var des = document.getElementById('gamePlaying');
-
-    des.setAttribute('src', 'img/des/third.gif');
-    des.setAttribute('onclick', 'nextGame();');
-}
-
+// NEXT GAME
 function nextGame(){
     round ++;
-
     play();
 }
 
